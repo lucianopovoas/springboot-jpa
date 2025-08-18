b363e745988174e05051a100cb65bc0273bd43f2
lucianopovoas <paixaoluciano69@gmail.com>
2025-08-18 17:08:10 -0300

    User e UserResource

---
+++ src/main/java/com/estudo/curso/entities/User.java
@@ -0,0 +1,76 @@
+package com.estudo.curso.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String nome, String email, String password, String phone) {
+        this.email = email;
+        this.id = id;
+        this.nome = nome;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
