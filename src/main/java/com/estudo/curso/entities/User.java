ec0e28b12ff23c76e4a87d2dfd12c0a096b20322
lucianopovoas <paixaoluciano69@gmail.com>
2025-08-19 16:04:47 -0300

    Inserido banco H2 e JPA

--- src/main/java/com/estudo/curso/entities/User.java
+++ src/main/java/com/estudo/curso/entities/User.java
@@ -1,10 +1,17 @@
 package com.estudo.curso.entities;
 
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
