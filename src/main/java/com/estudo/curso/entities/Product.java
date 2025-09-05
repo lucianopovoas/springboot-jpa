22148eb041492b315cdf306ecb8fda63753a7c21
lucianopovoas <paixaoluciano69@gmail.com>
2025-09-04 23:36:03 -0300

    Implementacao paayment, subtotal e total

--- src/main/java/com/estudo/curso/entities/Product.java
+++ src/main/java/com/estudo/curso/entities/Product.java
@@ -1,5 +1,6 @@
 package com.estudo.curso.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -22,6 +23,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -76,6 +80,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrder(){
+        Set<Order> orders = new HashSet<>();
+        for (OrderItem x : items){
+            orders.add(x.getOrder());
+        }
+        return orders;
     }
 
     @Override
